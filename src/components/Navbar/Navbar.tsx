e6a4ed41dccd347f327d184ee333701b57311da8
lorange <jglorange@gmail.com>
2022-08-27 21:07:32 +0000

    logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,12 @@
+import { createClient } from '@root/prismicio'
 import { motion, useAnimation } from 'framer-motion'
+import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Seperator from '~/components/Seperator'
 import Text from '~/components/Text'
+import { ImageType } from '~/types'
 
 type Props = {
   seperator?: boolean
@@ -12,6 +15,7 @@ type Props = {
 const Navbar = ({ seperator = true }: Props) => {
   const controls = useAnimation()
   const { asPath } = useRouter()
+  const [logo, setLogo] = useState<ImageType>()
 
   useEffect(() => {
     if (asPath !== '/') {
@@ -25,11 +29,20 @@ const Navbar = ({ seperator = true }: Props) => {
     }
   }, [asPath, controls])
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const client = createClient()
+      await client.getSingle('global_settings').then(result => setLogo(result.data.logo))
+    }
+    fetchData()
+  }, [])
+
   return (
     <motion.nav animate={controls} className="h-16 md:h-36 text-cream relative">
       <div className="container flex h-full justify-between relative">
         <Link href="/" passHref>
-          <motion.a initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.5 }} className="flex place-items-center">
+          <motion.a initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.5 }} className="grid grid-cols-[auto,1fr] gap-5 justify-start place-items-center">
+            {logo && <Image height={logo.dimensions.height} width={logo.dimensions.width} layout='fixed' alt='Gullplatan' src={logo.url} />}
             <Text variant="logo" as="h1">
               gullplatan
             </Text>
