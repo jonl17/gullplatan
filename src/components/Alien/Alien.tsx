ccca10d8108a2ab22a06e2da61ed817a80c4d440
Nonni Kosmos <jon@juni.is>
2023-02-10 16:40:37 +0000

    alien update

--- src/components/Alien/Alien.tsx
+++ src/components/Alien/Alien.tsx
@@ -3,11 +3,31 @@ import { motion } from 'framer-motion'
 import { useEffect, useState } from 'react'
 
 export default function Alien() {
-  const [mouse, setMouse] = useState({ x: -100, y: 2000 })
+  const [mouse, setMouse] = useState({ x: 500, y: 500 })
+  const [opacity, setOpacity] = useState(0)
 
   useEffect(() => {
     const callback = (e: MouseEvent) => {
-      setMouse({ x: e.x - 100, y: e.y + 100 })
+      const ex = e.x - 100
+      const ey = e.y - 100
+
+      const handleX = () => {
+        if (ex < 50) return 50
+        else if (ex > window.innerWidth - 150) return window.innerWidth - 200
+        else return ex
+      }
+
+      const handleY = () => {
+        if (ey < 50) return 50
+        else if (ey > window.innerHeight - 150) return window.innerHeight - 200
+        else return ey
+      }
+
+      const x = handleX()
+      const y = handleY()
+
+      setMouse({ x, y })
+      setOpacity(1)
     }
 
     document.addEventListener('mousemove', callback)
@@ -16,11 +36,12 @@ export default function Alien() {
 
   return (
     <motion.div
-      animate={{ x: mouse.x, y: mouse.y }}
-      transition={{ duration: 0.6, bounce: 0.95 }}
-      className="inline-block h-[150px] w-[150px] fixed top-0 left-0 pointer-events-none"
+      initial={{ opacity: 0 }}
+      animate={{ x: mouse.x, y: mouse.y, opacity }}
+      transition={{ duration: 0.6, damping: 900 }}
+      className="inline-block h-[100px] w-[100px] fixed top-0 left-0 pointer-events-none"
     >
-      <Image alt="Geimverann" src="/alien.png" height={150} width={150} />
+      <Image alt="Geimverann" src="/alien.png" height={100} width={100} />
     </motion.div>
   )
 }
